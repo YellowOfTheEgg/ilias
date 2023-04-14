24d7d7338262a9602b5cf77ef486adfada26431e
YellowOfTheEgg <sergej.korlakov@gmail.com>
2023-04-14 15:51:36 +0200

    cleanup

--- auth_login.go
+++ auth_login.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	loginPath string = "ilias.php?lang=de&client_id=UniRZ&cmd=post&cmdClass=ilstartupgui&cmdNode=11g&baseClass=ilStartUpGUI&rtoken="
+	loginPath string = "ilias.php?lang=de&client_id=UniRZ&cmd=post&cmdClass=ilstartupgui&cmdNode=112g&baseClass=ilStartUpGUI&rtoken="
 )
 
 func (auth *AuthService) Login(username string, password string) (*User, error) {
